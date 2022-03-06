9db71bb4205ddd47b0116e97fb9885fc0e912ab5
Brandon Tarrant <brandontarrant04@gmail.com>
2022-03-05 21:50:57 -0500

    Sent a text message using the Twilio API.

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from twilio.rest import Client
+import config
 
-Client()
+client = Client(config.account_sid, config.auth_token)
+
+call = client.messages.create(
+    to="689-610-0627",
+    from_="+19362263399",
+    body="This is our first message"
+)
